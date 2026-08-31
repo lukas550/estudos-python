38df5b02d39c63f3a49b0c73c736e1c7845c17d6
Lukas <xd3433660@gmail.com>
2026-08-31 19:47:35 -0300

    add: exercício com manipulação de arquivos em json

---
+++ exercicios-de-manipulacao-de-arquivos/ex004/ex003/ex003.py
@@ -0,0 +1,34 @@
+import json
+
+try:
+    with open("usuarios.json", "r", encoding="utf-8") as arquivo:
+        dados = json.load(arquivo)
+except FileNotFoundError, json.JSONDecodeError:
+    dados = []
+
+def salvar_arquivo(id, nome, status):
+    objeto = {"id": id, "nome": nome, "ativo": status}
+    dados.append(objeto)
+
+    with open("usuarios.json", "w", encoding="utf-8") as arquivo:
+        json.dump(dados, arquivo, ensure_ascii=False, indent=2)
+
+# Menu principal
+while True:
+    print("Digite 1 para adicionar nomes ao arquivo e 2 para encerrar!")
+    escolha = input("= ").strip()
+
+    if escolha == "1":
+        nome_do_usuario = input("Digite o nome de usuário: ").strip()
+        id_do_usuario = (input("Digite o id: ")).strip()
+        esta_ativo = input("Está ativo (sim/não): ").lower().strip()
+
+        status_do_usuario = True if esta_ativo in ["sim", "ss", "s"] else False
+
+        salvar_arquivo(id_do_usuario, nome_do_usuario, status_do_usuario)
+        print("\nSalvo!\n")
+    elif escolha == "2":
+        print("\nEncrrando...\n")
+        break
+    else:
+        print("\nDigite algo válido!\n")
